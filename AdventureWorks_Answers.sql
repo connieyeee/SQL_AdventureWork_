8639c29f94a7026c1e30b81eb431733c5aca2e26
Connie Ye <134975561+connieyeee@users.noreply.github.com>
2023-06-05 16:32:01 -0400

    Update AdventureWorks_Answers.sql

--- AdventureWorks_Answers.sql
+++ AdventureWorks_Answers.sql
@@ -7,9 +7,9 @@ Questions available at http://sqlzoo.net/wiki/AdventureWorks
 /*
 Show the first name and the email address of customer with CompanyName 'Bike World'
 */
-SELECT FirstName, EmailAddress
-FROM Customer
-WHERE CompanyName = 'Bike World';
+Select FirstName, EmailAddress
+from Customer
+where CompanyName = 'Bike World';
 
 Result:
 FirstName	EmailAddress
@@ -19,13 +19,13 @@ Kerim	kerim0@adventure-works.com
 /*
 Show the CompanyName for all customers with an address in City 'Dallas'.
 */
-SELECT CompanyName
-FROM Customer
-JOIN CustomerAddress
-ON Customer.CustomerID = CustomerAddress.CustomerID
-JOIN Address
-ON CustomerAddress.AddressID = Address.AddressID
-WHERE Address.City = 'Dallas';
+Select CompanyName
+from Customer
+join CustomerAddress
+on Customer.CustomerID = CustomerAddress.CustomerID
+Join Address
+on CustomerAddress.AddressID = Address.AddressID
+where Address.City = 'Dallas';
 
 Result:
 CompanyName
@@ -40,11 +40,11 @@ Rental Bikes
 /*
 How many items with ListPrice more than $1000 have been sold?
 */
-SELECT COUNT(*) AS Solditem
-FROM SalesOrderDetail
-JOIN Product
-ON SalesOrderDetail.ProductID = Product.ProductID
-WHERE Product.ListPrice > 1000;
+Select count(*) as Solditem
+from SalesOrderDetail
+join Product
+on SalesOrderDetail.ProductID = Product.ProductID
+where Product.ListPrice > 1000;
 
 Result:
 Solditem
@@ -54,11 +54,11 @@ Solditem
 /*
 Give the CompanyName of those customers with orders over $100000. Include the subtotal plus tax plus freight.
 */
-SELECT Customer.CompanyName
-FROM SalesOrderHeader
-JOIN Customer
-ON SalesOrderHeader.CustomerID = Customer.CustomerID
-WHERE SubTotal + TaxAmt + Freight > 100000;
+Select Customer.CompanyName
+from SalesOrderHeader
+join Customer
+on SalesOrderHeader.CustomerID = Customer.CustomerID
+where SubTotal + TaxAmt + Freight > 100000;
 
 Result:
 CompanyName
@@ -69,16 +69,16 @@ Metropolitan Bicycle Supply
 /*
 Find the number of left racing socks ('Racing Socks, L') ordered by CompanyName 'Riding Cycles'
 */
-SELECT SUM(SalesOrderDetail.OrderQty) As SumRacingSocks
-FROM SalesOrderDetail
-JOIN Product
-ON SalesOrderDetail.ProductID = Product.ProductID
-JOIN SalesOrderHeader
-ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
-JOIN Customer
-ON SalesOrderHeader.CustomerID = Customer.CustomerID
-WHERE Product.Name = 'Racing Socks, L'
-AND Customer.CompanyName = 'Riding Cycles';
+Select SUM(SalesOrderDetail.OrderQty) as SumRacingSocks
+from SalesOrderDetail
+join Product
+on SalesOrderDetail.ProductID = Product.ProductID
+join SalesOrderHeader
+on SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+join Customer
+on SalesOrderHeader.CustomerID = Customer.CustomerID
+where Product.Name = 'Racing Socks, L'
+and Customer.CompanyName = 'Riding Cycles';
 
 Result:
 SumRacingSocks
@@ -88,9 +88,9 @@ SumRacingSocks
 /*
 A "Single Item Order" is a customer order where only one item is ordered. Show the SalesOrderID and the UnitPrice for every Single Item Order.
 */
-SELECT SalesOrderID, UnitPrice
-FROM SalesOrderDetail
-WHERE OrderQty = 1;
+Select SalesOrderID, UnitPrice
+from SalesOrderDetail
+where OrderQty = 1;
 
 Result:
 SalesOrderID	UnitPrice
@@ -105,17 +105,17 @@ Results truncated. Only the first 5 rows have been shown.
 /*
 Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
 */
-SELECT Product.name, Customer.CompanyName
-FROM ProductModel
-JOIN Product
-ON ProductModel.ProductModelID = Product.ProductModelID
-JOIN SalesOrderDetail
-ON SalesOrderDetail.ProductID = Product.ProductID
-JOIN SalesOrderHeader
-ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
-JOIN Customer
-ON SalesOrderHeader.CustomerID = Customer.CustomerID
-WHERE ProductModel.Name = 'Racing Socks';
+Select Product.name, Customer.CompanyName
+from ProductModel
+join Product
+on ProductModel.ProductModelID = Product.ProductModelID
+join SalesOrderDetail
+on SalesOrderDetail.ProductID = Product.ProductID
+join SalesOrderHeader
+on SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+join Customer
+on SalesOrderHeader.CustomerID = Customer.CustomerID
+where ProductModel.Name = 'Racing Socks';
 
 Result:
 name	CompanyName
@@ -134,16 +134,16 @@ Racing Socks, L	Remarkable Bike Store
 /*
 Show the product description for culture 'fr' for product with ProductID 736.
 */
-SELECT ProductDescription.Description
-FROM ProductDescription
-JOIN ProductModelProductDescription
-ON ProductDescription.ProductDescriptionID = ProductModelProductDescription.ProductDescriptionID
-JOIN ProductModel
-ON ProductModelProductDescription.ProductModelID = ProductModel.ProductModelID
-JOIN Product
-ON ProductModel.ProductModelID = Product.ProductModelID
-WHERE ProductModelProductDescription.culture = 'fr'
-AND Product.ProductID = '736';
+Select ProductDescription.Description
+from ProductDescription
+join ProductModelProductDescription
+on ProductDescription.ProductDescriptionID = ProductModelProductDescription.ProductDescriptionID
+join ProductModel
+on ProductModelProductDescription.ProductModelID = ProductModel.ProductModelID
+join Product
+on ProductModel.ProductModelID = Product.ProductModelID
+where ProductModelProductDescription.culture = 'fr'
+and Product.ProductID = '736';
 
 Result:
 Description
@@ -153,16 +153,16 @@ Le cadre LL en aluminium offre une conduite confortable, une excellente absorpti
 /*
 Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. For each order show the CompanyName and the SubTotal and the total weight of the order.
 */
-SELECT Customer.CompanyName, SalesOrderHeader.SubTotal, SUM(SalesOrderDetail.OrderQty * Product.weight) as SumWeight
-FROM Product
-JOIN SalesOrderDetail
-ON Product.ProductID = SalesOrderDetail.ProductID
-JOIN SalesOrderHeader
-ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesorderID
-JOIN Customer
-ON SalesOrderHeader.CustomerID = Customer.CustomerID
-GROUP BY SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal, Customer.CompanyName
-ORDER BY SalesOrderHeader.SubTotal DESC;
+Select Customer.CompanyName, SalesOrderHeader.SubTotal, SUM(SalesOrderDetail.OrderQty * Product.weight) as SumWeight
+from Product
+join SalesOrderDetail
+on Product.ProductID = SalesOrderDetail.ProductID
+join SalesOrderHeader
+on SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesorderID
+join Customer
+on SalesOrderHeader.CustomerID = Customer.CustomerID
+group by SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal, Customer.CompanyName
+group by SalesOrderHeader.SubTotal desc;
 
 Result:
 CompanyName	SubTotal	SumWeight
@@ -171,27 +171,133 @@ Metropolitan Bicycle Supply	98278.69	679588.02
 Bulk Discount Store	88812.86	34813.05
 Eastside Department Store	83858.43	565638.72
 Riding Cycles	78029.69	504095.33
-
-Only the first 5 rows have been shown.
+...
+#Only the first 5 rows have been shown.
 
 -- #10
 /*
 How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
 */
-SELECT SUM(SalesOrderDetail.OrderQty) as SoldQty
-FROM ProductCategory
-JOIN Product
-ON ProductCategory.ProductCategoryID = Product.ProductCategoryID
-JOIN SalesOrderDetail
-ON Product.ProductID = SalesOrderDetail.ProductID
-JOIN SalesOrderHeader
-ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesorderID
-JOIN Address
-ON SalesOrderHeader.ShipToAddressID = Address.AddressID
-WHERE Address.City = 'London'
-AND ProductCategory.Name = 'Cranksets';
+Select SUM(SalesOrderDetail.OrderQty) as SoldQty
+from ProductCategory
+join Product
+on ProductCategory.ProductCategoryID = Product.ProductCategoryID
+join SalesOrderDetail
+on Product.ProductID = SalesOrderDetail.ProductID
+join SalesOrderHeader
+on SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesorderID
+join Address
+on SalesOrderHeader.ShipToAddressID = Address.AddressID
+where Address.City = 'London'
+and ProductCategory.Name = 'Cranksets';
 
 Result:
 SoldQty
 9
+
+-- #11
+/*
+For every customer with a 'Main Office' in Dallas show AddressLine1 of the 'Main Office' and AddressLine1 of the 'Shipping' address - if there is no shipping address leave it blank. Use one row per customer.
+*/
+Select CompanyName,
+MAX(case when AddressType = 'Main Office' then AddressLine1 else '' end) as MainOfficeAdress,
+MAX(case when AddressType = 'Shipping' then AddressLine1 else '' end) as ShippingAddress
+from Address
+join CustomerAddress
+on Address.AddressID = CustomerAddress.AddressID
+join Customer
+on CustomerAddress.CustomerID = Customer.CustomerID
+where Address.City = 'Dallas'
+group by Customer.CompanyName
+
+Result:
+CompanyName	MainOfficeAdr..	ShippingAddre..
+Elite Bikes	Po Box 8259024	9178 Jumping St.
+Rental Bikes	99828 Routh Street, Suite 825	
+Third Bike Store	2500 North Stemmons Freeway	
+Town Industries	P.O. Box 6256916	
+Unsurpassed Bikes	Po Box 8035996
+
+-- #12
+/*
+For each order show the SalesOrderID and SubTotal calculated three ways:
+A) From the SalesOrderHeader
+B) Sum of OrderQty*UnitPrice
+C) Sum of OrderQty*ListPrice
+*/
+Select SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal as ST1,
+sum(OrderQty*UnitPrice) as ST2, sum(OrderQty * ListPrice) as ST3
+from SalesOrderHeader
+join SalesOrderDetail 
+on SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+join Product 
+on SalesOrderDetail.ProductID = Product.ProductID
+group by SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal;
+
+Result:
+SalesOrderID	ST1	ST2	ST3
+71774	880.35	713.8	1189.66
+71776	78.81	63.9	106.5
+71780	38418.69	30600.81	56651.56
+71782	39785.33	33319.68	55533.31
+71783	83858.43	68141.99	121625.43
+...
+#Only the first 5 rows have been shown.
+
+-- #13
+/*
+Show the best selling item by value.
+*/
+Select Product.Name,
+sum(SalesOrderDetail.OrderQTY * SalesOrderDetail.UnitPrice) as TotalValue
+from Product
+join SalesOrderDetail
+on Product.ProductID = SalesOrderDetail.ProductID
+group by Product.Name
+Order by TotalValue desc;
+
+Result:
+Name	TotalValue
+Touring-1000 Blue, 60	37191.44
+
+-- #14
+/*
+Show how many orders are in the following ranges (in $):
+
+    RANGE      Num Orders      Total Value
+    0-  99
+  100- 999
+ 1000-9999
+10000-
+*/
+Select a.range as RANGE, count(a.Total) as 'Num orders', sum(a.Total) as 'Total Value'
+from (select
+      case 
+      when SalesOrderDetail.UnitPrice * SalesOrderDetail.OrderQty between 0 and 99
+      then '0 - 99'
+      when SalesOrderDetail.UnitPrice * SalesOrderDetail.OrderQty between 100 and 999
+      then '100-999'
+      when SalesOrderDetail.UnitPrice * SalesOrderDetail.OrderQty between 1000 and 9999
+      then '1000-9999'
+      when SalesOrderDetail.UnitPrice * SalesOrderDetail.OrderQty > 10000
+      then '10000-'
+      else ''
+      end as range,
+      SalesOrderDetail.UnitPrice * SalesOrderDetail.OrderQty AS Total
+      from SalesOrderDetail)a
+group by a.range
+
+Result:
+RANGE	Num orders	Total Value
+0 - 99	129	6576.02
+100-999	225	90859.33
+1000-9999	138	433813.21
+10000-	8	113661.13
+
+-- #15
+/*
+Identify the three most important cities. Show the break down of top level product category against city.
+*/
+
+
 
